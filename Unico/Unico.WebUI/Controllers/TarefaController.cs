57d6204ac913ff25533f1bbf9fd665a3ad098276
Claudenes Renato Soares de Almeida <claudenes.almeida@grupoaguasdobrasil.com.br>
2024-09-24 20:29:31 -0300

    Implementacao do WEBUI

---
+++ Unico/Unico.WebUI/Controllers/TarefaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unico.Application.Interfaces;
+
+namespace Unico.WebUI.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private readonly ITarefaService _service;
+        public TarefaController(ITarefaService service)
+        {
+            _service = service;
+
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var tarefas = _service.ListAll();
+            return View(tarefas);
+        }
+    }
+}
